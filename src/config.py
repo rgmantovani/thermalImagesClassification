84c4f490482be8116cb3fb273987f4e48aa63b7a
Rafael Gomes Mantovani <rgmantovani@gmail.com>
2025-05-04 18:14:44 -0300

    adding 10 seeds with results for CNN

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ NUMBER_OF_CLASSES = 2
 
 AVAILABLE_TYPES_OF_IMAGE = ["raw", "rgb"]
 
-AVAILABLE_MODELS = ["cnn"] #, "vgg19", "lwcnn", "resnet"]
+AVAILABLE_MODELS = ["lwcnn"] #, "vgg19", "cnn", "resnet"]
 
 AVAILABLE_ML_ALGORITHMS = ["rf", "dt", "bagging", "knn", "svm", "ridge"]
 
